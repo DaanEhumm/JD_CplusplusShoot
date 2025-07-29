0d015cb687858d967dc9fe9c765cedaa6f763117
DaanEhumm <daan.hummeling@gmail.com>
2025-07-29 14:12:10 +0200

    Bullet Textures/ pistol expansion
    pistol scripts added, bullet textures added, pistol not jet working weapon switch,ammo/reload, and pistol adding is next

--- JD_ShootSFML/JD_ShootSFML/Bullet.h
+++ JD_ShootSFML/JD_ShootSFML/Bullet.h
@@ -3,13 +3,13 @@
 
 class Bullet {
 public:
-    Bullet(sf::Vector2f pos, sf::Vector2f velocity);
+    Bullet(sf::Vector2f pos, sf::Vector2f velocity, const sf::Texture& texture);
 
     void update(float deltaTime);
     void draw(sf::RenderWindow& window);
     bool isOffScreen(const sf::RenderWindow& window) const;
 
 private:
-    sf::CircleShape shape;
+    sf::Sprite sprite;
     sf::Vector2f velocity;
 };
